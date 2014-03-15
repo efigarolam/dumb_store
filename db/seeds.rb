c6983e267e35d28003779e213a6fae417f5c435c
Eduardo Figarola <eduardo.figarola@crowdint.com>
2014-03-15 12:43:35 -0600

    Some seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+google = Provider.create name: 'Google Inc', email: 'google@gmail.com'
+apple  = Provider.create name: 'Apple Inc', email: 'apple@mac.com'
+
+nexus5 = Product.create name: 'Nexus 5', description: 'Android', price: 350.0, provider: google
+nexus7 = Product.create name: 'Nexus 7', description: 'Android', price: 250.0, provider: google
+iphone5 = Product.create name: 'iPhone 5s', description: 'iOS', price: 400.0, provider: apple
+ipad = Product.create name: 'iPad mini', description: 'iPad', price: 300.0, provider: apple
